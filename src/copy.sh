9eb44cf7202f224c7e7ec3f3447ba6d6f52901eb
David Urbina <davidurbina.dev@gmail.com>
2023-04-12 14:15:09 -0500

    Created copy/paste scripts using xclip

---
+++ src/copy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Check if xclip is installed
+if ! command -v xclip > /dev/null; then
+    echo "xclip is not installed"
+    exit 1
+fi
+
+# Use xclip to copy shell output to clipboard
+xclip -selection clipboard
