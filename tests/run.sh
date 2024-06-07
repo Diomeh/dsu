20f9741677981e799e52189e9556f42e30650b71
David Urbina <davidurbina.dev@gmail.com>
2024-06-06 21:03:50 -0500

    Added test cases for backup

--- tests/run.sh
+++ tests/run.sh
@@ -20,9 +20,9 @@ print() {
     local message="$2"
     
     case "$color" in
-        "e") echo -e "${RED}[ERROR] ${message}${NC}" ;;
+        "e") echo -e "${RED}[ERROR] ${message}${NC}" >&2 ;;
         "s") echo -e "${GREEN}[SUCCESS] ${message}${NC}" ;;
-        "w") echo -e "${YELLOW}[WARNING] ${message}${NC}" ;;
+        "w") echo -e "${YELLOW}[WARNING] ${message}${NC}" >&2 ;;
         "i") echo -e "${BLUE}[INFO] ${message}${NC}" ;;
         *) echo "$message" ;;
     esac
@@ -47,7 +47,7 @@ arg_parse() {
             exit 0
             ;;
         *)
-            print "e" "Unknown option: $1" >&2
+            print "e" "Unknown option: $1"
             usage
             exit 1
             ;;
@@ -56,21 +56,101 @@ arg_parse() {
 }
 
 test_backup() {
-    print "w" "backup: test not implemented\n"
+    local workdir="$TEST_DIR/backup"
+    local binary="$SRC_DIR/backup.sh"
+    local outfile="$TMP_DIR/backup.out"
+    local r_file="r.txt.2024-06-06_17-47-45.backup"
+    local b_file="b.txt"
+    local result
+
+    print "i" "backup: running tests"
+
+    touch "$outfile"
+
+    print "i" "backup: testing backup operation"
+
+    # Perform -b operation, get script exit code and redirect output to a file
+    "$binary" "-b" "./backup/$b_file" "./backup" >> "$outfile"
+    local b_exit_code=$?
+
+    # Check the exit code of the backup operation
+    if [ "$b_exit_code" -ne 0 ]; then
+        print "e" "backup: test failed during backup operation with exit code $b_exit_code"
+        print "i" "backup: test output:"
+        cat "$outfile"
+        return 1
+    fi
+
+    # Verify if file with format b.txt.YYYY-MM-DD_HH-MM-SS.backup exists
+    local file="$(find "$workdir" -name "b.txt.*.backup")"
+    if [ -z "$file" ]; then
+        print "e" "backup: backup operation failed to create target file"
+        print "i" "backup: test output:"
+        cat "$outfile"
+        return 1
+    else
+        # Compare the backup file with the original file
+        result=$(diff -qw "$file" "$workdir/$b_file" | wc -l)
+        if [ "$result" -ne 0 ]; then
+            print "e" "backup: backup file does not match original file"
+            print "i" "backup: test output:"
+            cat "$outfile"
+            return 1
+        else
+            print "s" "backup: backup operation passed"
+            rm "$file"
+        fi
+    fi
+
+    print "i" "backup: testing restore operation"
+
+    # Perform -r operation
+    "$binary" "-r" "./backup/$r_file" "./backup" >> "$outfile"
+    local r_exit_code=$?
+    
+    # Check the exit code of the restore operation
+    if [ "$r_exit_code" -ne 0 ]; then
+        print "e" "backup: test failed during restore operation with exit code $r_exit_code"
+        print "i" "backup: test output:"
+        cat "$outfile"
+        return 1
+    fi
+
+    # Verify the restored file exists
+    if [ ! -e "$workdir/r.txt" ]; then
+        print "e" "backup: restore operation failed to create target file"
+        print "i" "backup: test output:"
+        cat "$outfile"
+        return 1
+    else
+        # Compare the restored file with the original file
+        result=$(diff -qw "$workdir/r.txt" "$workdir/$r_file" | wc -l)
+        if [ "$result" -ne 0 ]; then
+            print "e" "backup: restored file does not match original file"
+            print "i" "backup: test output:"
+            cat "$outfile"
+            return 1
+        else
+            print "s" "backup: restore operation passed"
+            rm "$workdir/r.txt"
+        fi
+    fi
+
+    print "s" "backup: all tests passed"
 }
 
 test_cln() {
-    print "w" "cln: test not implemented\n"
+    print "w" "cln: test not implemented"
 }
 
 test_copy() {
-    print "w" "copy: test not implemented\n"
+    print "w" "copy: test not implemented"
 }
 
 test_hog() {
     local result
 
-    print "i" "Running hog test"
+    print "i" "hog: running test"
 
     # Execute hog.sh and redirect output to a file
     "$SRC_DIR/hog.sh" "./hog" | egrep --invert-match expected > "$TMP_DIR/hog.out"
@@ -79,10 +159,10 @@ test_hog() {
     result=$(diff -qw "$TMP_DIR/hog.out" "$TEST_DIR/hog/.expected" | wc -l)
 
     if [ "$result" -eq 0 ]; then
-        print "s" "Hog: test passed"
+        print "s" "hog: test passed"
     else
         echo
-        print "e" "Hog: test failed"
+        print "e" "hog: test failed"
         print "i" "Expected output:"
         cat "$TEST_DIR/hog/.expected"
         echo
@@ -90,16 +170,15 @@ test_hog() {
         cat "$TMP_DIR/hog.out"
     fi
 
-    echo
     return $result
 }
 
 test_paste() {
-    print "w" "paste: test not implemented\n"
+    print "w" "paste: test not implemented"
 }
 
 test_xtract() {
-    print "w" "xtract: test not implemented\n"
+    print "w" "xtract: test not implemented"
 }
 
 main() {
