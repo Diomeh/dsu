b3b342f83fb7b7e0d51b093ace5e1f364acf1d6c
David Urbina <davidurbina.dev@gmail.com>
2024-06-24 20:33:41 -0500

    Implemented `copy`

--- src/copy.rs
+++ src/copy.rs
@@ -1,13 +1,21 @@
-use color_eyre::eyre::Result;
+use color_eyre::eyre::{eyre, Result};
+use copypasta::{ClipboardContext, ClipboardProvider};
+use std::io::{self, Read};
+use tracing::trace;
 
-use crate::{
-    DRunnable,
-    CopyArgs,
-};
+use crate::{CopyArgs, DRunnable};
 
 impl DRunnable for CopyArgs {
     fn run(&mut self) -> Result<()> {
-        println!("Running CopyArgs");
-        Ok(())
+        trace!("args: {self:?}");
+
+        let mut input = String::new();
+        if let Err(err) = io::stdin().read_to_string(&mut input) {
+            return Err(eyre!("Failed to read from stdin: {}", err));
+        }
+
+        ClipboardContext::new()
+            .and_then(|mut ctx| ctx.set_contents(input))
+            .map_err(|err| eyre!("Failed to set clipboard contents: {}", err))
     }
 }
