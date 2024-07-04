19cfac64824fb454837ab3fec3e538e6f9d46f2d
David Urbina <davidurbina.dev@gmail.com>
2024-07-04 16:13:26 -0500

    Added support for wayland session copy/paste

--- sh/copy.sh
+++ sh/copy.sh
@@ -2,11 +2,26 @@
 #
 # -*- mode: shell-script -*-
 
-# Check if xclip is installed
-if ! command -v xclip > /dev/null; then
-    echo "xclip is not installed"
+# Determine if user is running Wayland or Xorg
+if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+  # Check if wl-copy is installed
+  if ! command -v wl-copy >/dev/null; then
+    echo "[ERROR] wl-copy is not installed" >&2
     exit 1
-fi
+  fi
 
-# Use xclip to copy shell output to clipboard
-xclip -selection clipboard
+  # Use wl-copy to copy shell output to clipboard
+  wl-copy
+elif [ "$XDG_SESSION_TYPE" = "x11" ]; then
+  # Check if xclip is installed
+  if ! command -v xclip >/dev/null; then
+    echo "[ERROR] xclip is not installed" >&2
+    exit 1
+  fi
+
+  # Use xclip to copy shell output to clipboard
+  xclip -selection clipboard
+else
+  echo "[ERROR] Unknown session type: $XDG_SESSION_TYPE" >&2
+  exit 1
+fi
--- sh/paste.sh
+++ sh/paste.sh
@@ -2,11 +2,26 @@
 #
 # -*- mode: shell-script -*-
 
-# Check if xclip is installed
-if ! command -v xclip > /dev/null; then
-    echo "xclip is not installed"
+# Determine if user is running Wayland or Xorg
+if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+  # Check if wl-paste is installed
+  if ! command -v wl-paste >/dev/null; then
+    echo "[ERROR] wl-paste is not installed" >&2
     exit 1
-fi
+  fi
 
-# Paste clipboard contents to stdin
-xclip -o -sel clip
+  # Paste clipboard contents to stdin
+  wl-paste
+elif [ "$XDG_SESSION_TYPE" = "x11" ]; then
+  # Check if xclip is installed
+  if ! command -v xclip >/dev/null; then
+    echo "[ERROR] xclip is not installed" >&2
+    exit 1
+  fi
+
+  # Paste clipboard contents to stdin
+  xclip -o -sel clip
+else
+  echo "[ERROR] Unknown session type: $XDG_SESSION_TYPE" >&2
+  exit 1
+fi
