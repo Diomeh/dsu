45e820619a33386bb028a6ee961639cbdf4c1a89
David Urbina <davidurbina.dev@gmail.com>
2025-05-25 10:37:14 -0500

    chore: add rust formatter tools

--- shell.nix
+++ shell.nix
@@ -2,6 +2,8 @@
 pkgs.mkShellNoCC {
   packages = with pkgs; [
     bacon # Background rust code checker
+    rustfmt # Rust code checker
+    clippy # Bunch of lints to catch common mistakes and improve your Rust code
     shellcheck # Shell script linter
     shunit2 # Shell script unit testing framework
     shfmt # Shell script formatter
